ccf2c2fa821fcc73a0b3de225ded1f1a6478119b
LP <grofit1@yahoo.com>
2017-03-17 13:40:27 +0000

    Added endpoint interfaces
    Added interfaces for endpoints to allow sending and recieving data, this opens up a way for people to do their own actions within there.

---
+++ src/Assets/Persistity/Endpoints/IReceiveData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Persistity.Endpoints
+{
+    public interface IReceiveData
+    { }
+
+    public interface IReceiveData<TOut> : ISendData
+    {
+        void Execute(Action<TOut> onSuccess, Action<Exception> onError);
+    }
+
+    public interface IReceiveData<TOut, TIn> : ISendData
+    {
+        void Execute(TIn data, Action<TOut> onSuccess, Action<Exception> onError);
+    }
+
+}
