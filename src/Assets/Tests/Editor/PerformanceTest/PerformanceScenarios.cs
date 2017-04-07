534f169f5f83559dbde6284bfad75d17059a33d0
LP <grofit1@yahoo.com>
2017-04-07 10:01:43 +0100

    Updated performance tests
    Have updated performance tests to show serialization for all formats and deserialization as well as splitting out the primitive logic from serializers for re-use in type handlers.

--- src/Assets/Tests/Editor/PerformanceTest/PerformanceScenarios.cs
+++ src/Assets/Tests/Editor/PerformanceTest/PerformanceScenarios.cs
@@ -1,23 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using Persistity;
 using Persistity.Mappings.Mappers;
 using Persistity.Mappings.Types;
 using Persistity.Registries;
+using Persistity.Serialization;
 using Persistity.Serialization.Binary;
+using Persistity.Serialization.Json;
+using Persistity.Serialization.Xml;
+using Tests.Editor.Helpers;
 
 namespace Tests.Editor.PerformanceTest
 {
     [TestFixture]
     public class PerformanceScenarios
     {
-        const int Iterations = 100000;
+        const int Iterations = 10000;
+
+        private void RunSerializeAndDeserializeStep(object model, object modelList, ISerializer serializer, IDeserializer deserializer)
+        {
+            var startTime = DateTime.Now;
+            for (var i = 0; i < Iterations; i++)
+            { serializer.Serialize(model); }
+            var endTime = DateTime.Now;
+            var totalTime = endTime - startTime;
+            var average = totalTime.TotalMilliseconds / Iterations;
+            Console.WriteLine("Serialized {0} Entities in {1} with {2}ms average", Iterations, totalTime, average);
+
+            startTime = DateTime.Now;
+            var output = serializer.Serialize(modelList);
+            endTime = DateTime.Now;
+            totalTime = endTime - startTime;
+            Console.WriteLine("Serialized Large Entity with {0} elements in {1}", Iterations, totalTime);
+            Console.WriteLine("Large Entity Size {0}bytes", output.AsBytes.Length);
+
+            startTime = DateTime.Now;
+            deserializer.Deserialize(output);
+            endTime = DateTime.Now;
+            totalTime = endTime - startTime;
+            Console.WriteLine("Deserialized Large Entity with {0} elements in {1}", Iterations, totalTime);
+        }
+
+        private void RunStepsForFormats(MappingRegistry mappingRegistry, object model, object modelList)
+        {
+            var typeCreator = new TypeCreator();
+
+            ISerializer serializer;
+            IDeserializer deserializer;
+            DataObject warmupOutput;
+
+            // Binary Warmup
+            serializer = new BinarySerializer(mappingRegistry);
+            serializer.Serialize(model);
+            warmupOutput = serializer.Serialize(modelList);
+            deserializer = new BinaryDeserializer(mappingRegistry, typeCreator);
+            deserializer.Deserialize(warmupOutput);
+
+            Console.WriteLine("");
+            Console.WriteLine("Binary Serializing");
+            RunSerializeAndDeserializeStep(model, modelList, serializer, deserializer);
+            Console.WriteLine("");
+
+            // JSON Warmup
+            serializer = new JsonSerializer(mappingRegistry);
+            serializer.Serialize(model);
+            warmupOutput = serializer.Serialize(modelList);
+            deserializer = new JsonDeserializer(mappingRegistry, typeCreator);
+            deserializer.Deserialize(warmupOutput);
+
+            Console.WriteLine("");
+            Console.WriteLine("Json Serializing");
+            RunSerializeAndDeserializeStep(model, modelList, serializer, deserializer);
+            Console.WriteLine("");
+
+            // XML Warmup
+            serializer = new XmlSerializer(mappingRegistry);
+            serializer.Serialize(model);
+            warmupOutput = serializer.Serialize(modelList);
+            deserializer = new XmlDeserializer(mappingRegistry, typeCreator);
+            deserializer.Deserialize(warmupOutput);
+
+            Console.WriteLine("");
+            Console.WriteLine("Xml Serializing");
+            RunSerializeAndDeserializeStep(model, modelList, serializer, deserializer);
+            Console.WriteLine("");
+        }
 
         [Test]
         public void test_performance_with_simple_models()
         {
-            var person = new Person
+            var model = new Person
             {
                 Age = 99999,
                 FirstName = "Windows",
@@ -25,35 +98,44 @@ namespace Tests.Editor.PerformanceTest
                 Sex = Sex.Male,
             };
 
-            var personList = new PersonList();
-            personList.People = Enumerable.Range(Iterations, Iterations).Select(x => new Person { Age = x, FirstName = "Windows", LastName = "Server", Sex = Sex.Female }).ToArray();
+            var modelList = new PersonList();
+            modelList.Models = Enumerable.Range(Iterations, Iterations).Select(x => new Person { Age = x, FirstName = "Windows", LastName = "Server", Sex = Sex.Female }).ToArray();
 
             var typeAnalyzer = new TypeAnalyzer();
             var typeMapper = new EverythingTypeMapper(typeAnalyzer);
             var mappingRegistry = new MappingRegistry(typeMapper);
-            var serializer = new BinarySerializer(mappingRegistry);
 
-            // Warmup
-            serializer.Serialize(person);
-            serializer.Serialize(personList);
+            RunStepsForFormats(mappingRegistry, model, modelList);
+        }
 
-            Console.WriteLine("Serializing");
+        [Test]
+        public void test_performance_with_complex_models()
+        {
+            var model = SerializationTestHelper.GeneratePopulatedModel();
 
-            var startTime = DateTime.Now;
-            for (var i = 0; i < Iterations; i++)
-            { serializer.Serialize(person); }
-            var endTime = DateTime.Now;
-            var totalTime = endTime - startTime;
-            var average = totalTime.TotalMilliseconds / Iterations;
-            Console.WriteLine("Serialized {0} Entities in {1} with {2}ms average", Iterations, totalTime, average);
+            var modelList = new ComplexModelList();
+            modelList.Models = Enumerable.Range(Iterations, Iterations).Select(x => SerializationTestHelper.GeneratePopulatedModel()).ToArray();
 
-            startTime = DateTime.Now;
-            var output = serializer.Serialize(personList);
-            endTime = DateTime.Now;
-            totalTime = endTime - startTime;
-            Console.WriteLine("Serialized Large Entity with {0} elements in {1}", Iterations, totalTime);
-            Console.WriteLine("Large Entity Size {0}bytes", output.AsBytes.Length);
+            var typeAnalyzer = new TypeAnalyzer();
+            var typeMapper = new EverythingTypeMapper(typeAnalyzer);
+            var mappingRegistry = new MappingRegistry(typeMapper);
 
+            RunStepsForFormats(mappingRegistry, model, modelList);
+        }
+
+        [Test]
+        public void test_performance_with_dynamic_models()
+        {
+            var model = SerializationTestHelper.GeneratePopulatedDynamicTypesModel();
+
+            var modelList = new DynamicModelList();
+            modelList.Models = Enumerable.Range(Iterations, Iterations).Select(x => SerializationTestHelper.GeneratePopulatedDynamicTypesModel()).ToArray();
+
+            var typeAnalyzer = new TypeAnalyzer();
+            var typeMapper = new EverythingTypeMapper(typeAnalyzer);
+            var mappingRegistry = new MappingRegistry(typeMapper);
+
+            RunStepsForFormats(mappingRegistry, model, modelList);
         }
     }
 }
