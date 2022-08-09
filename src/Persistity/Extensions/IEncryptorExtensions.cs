4d323d9525017fd552d10f1fa45ff537b4ed0b54
LP <grofit1@yahoo.com>
2022-08-09 15:08:01 +0100

    Added extension methods for encryptor

---
+++ src/Persistity/Extensions/IEncryptorExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+using Persistity.Encryption;
+
+namespace Persistity.Extensions
+{
+    public static class IEncryptorExtensions
+    {
+        public static string Encrypt(this IEncryptor encryptor, string textToEncrypt)
+        {
+            var textAsBytes = Encoding.UTF8.GetBytes(textToEncrypt);
+            var encryptedData = encryptor.Encrypt(textAsBytes);
+            return Convert.ToBase64String(encryptedData);
+        }
+    
+        public static string Decrypt(this IEncryptor encryptor, string encryptedText)
+        {
+            var textAsBytes = Convert.FromBase64String(encryptedText);
+            var encryptedData = encryptor.Decrypt(textAsBytes);
+            return Encoding.UTF8.GetString(encryptedData);
+        }
+    }
+}
